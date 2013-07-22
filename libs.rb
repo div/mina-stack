27d76907d2f45a73a19c7504082879f25817a1dd
Igor Davydov <iskiche@gmail.com>
2013-07-23 00:37:28 +0400

    Add libcurl + libxml

---
+++ libs.rb
@@ -0,0 +1,19 @@
+namespace :libcurl do
+
+  desc "Install the latest release of libcurl"
+  task :install do
+    invoke :sudo
+    queue "sudo apt-get -y install curl libcurl3 libcurl3-dev"
+  end
+
+end
+
+namespace :libxml do
+
+  desc "Install the latest release of libxml"
+  task :install do
+    invoke :sudo
+    queue "sudo apt-get -y install libxml2 libxml2-dev libxslt1-dev"
+  end
+
+end
