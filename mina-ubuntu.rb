e82e3c8acf09df3d111b3ac5e316261ca8709dd2
Igor Davydov <iskiche@gmail.com>
2013-07-23 01:08:02 +0400

    lambda

--- mina-ubuntu.rb
+++ mina-ubuntu.rb
@@ -24,4 +24,4 @@ server_stack.each do |service|
   require "mina-ubuntu/#{service}"
 end
 
-set_default :rails_socket, { server_stack.include?(:puma) ? puma_socket : unicorn_socket }
+set_default :rails_socket, lambda { server_stack.include?(:puma) ? puma_socket : unicorn_socket }
