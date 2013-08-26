2d25a46cb168a9065d27aaccfb883762d2ced0d0
Igor Davydov <iskiche@gmail.com>
2013-08-26 15:39:45 +0400

    Require all the recepies

--- mina-ubuntu.rb
+++ mina-ubuntu.rb
@@ -3,7 +3,4 @@ require 'mina-ubuntu/base'
 require 'mina-ubuntu/setup'
 
 Dir['config/servers/*.rb'].each { |f| load f }
-
-server_stack.each do |service|
-  require "mina-ubuntu/recipes/#{service}"
-end
+Dir[File.dirname(__FILE__) + '/recipes/*.rb'].each {|file| require file }
