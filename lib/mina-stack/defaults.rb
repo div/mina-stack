99ebfdfa4a909488ef1efb4158ba103d00e17cbf
Igor Davydov <iskiche@gmail.com>
2013-09-21 06:22:27 +0400

    one more typo

--- lib/mina-stack/defaults.rb
+++ lib/mina-stack/defaults.rb
@@ -22,7 +22,7 @@ task :defaults do
   set_default :puma_pid,              "#{pids_path}/puma.pid"
   set_default :puma_log,              "#{logs_path}/puma.log"
   set_default :puma_socket,           "#{sockets_path}/puma.sock"
-  set_default :puma_state,            "#{sockets_path}/puma.state" }
+  set_default :puma_state,            "#{sockets_path}/puma.state"
   set_default :puma_workers,          2
 
   set_default :unicorn_name,          "unicorn_#{app_namespace!}"
