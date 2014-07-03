a62a96b7222ef938713eabedf2ac6fc51cffc665
Igor Davydov <iskiche@gmail.com>
2014-07-03 04:41:37 +0400

    use mina-sidekiq gem instead

--- lib/recipes/sidekiq.rb
+++ lib/recipes/sidekiq.rb
@@ -14,28 +14,29 @@ namespace :sidekiq do
     queue "sudo mv /tmp/sidekiq_conf #{sidekiq_upstart}"
   end
 
-  %w[quiet].each do |command|
-    desc "#{command.capitalize} sidekiq"
-    task command do
-      queue %{ if [ -f #{sidekiq_pid} ]; then
-        echo "-----> #{command.capitalize} sidekiq"
-        #{echo_cmd %{(cd #{deploy_to}/#{current_path} && #{sidekiqctl_cmd} #{command} #{sidekiq_pid})} }
-        fi }
-    end
-  end
+  # %w[quiet].each do |command|
+  #   desc "#{command.capitalize} sidekiq"
+  #   task command do
+  #     queue %{ if [ -f #{sidekiq_pid} ]; then
+  #       echo "-----> #{command.capitalize} sidekiq"
+  #       #{echo_cmd %{(cd #{deploy_to}/#{current_path} && #{sidekiqctl_cmd} #{command} #{sidekiq_pid})} }
+  #       fi }
+  #   end
+  # end
 
-  desc "Restart Sidekiq"
-  task :restart do
-    invoke :'sidekiq:stop'
-    invoke :'sidekiq:start'
-  end
+  # desc "Restart Sidekiq"
+  # task :restart do
+  #   invoke :'sidekiq:stop'
+  #   invoke :'sidekiq:start'
+  # end
 
-  %w[start stop].each do |command|
-    desc "#{command.capitalize} sidekiq"
-    task command do
-      # need to get rid of sudo here: have to figure out how it works with upstart
-      queue "sudo service #{sidekiq_name} #{command}"
-      queue  %[echo "-----> #{command.capitalize} sidekiq."]
-    end
-  end
+  # %w[start stop].each do |command|
+  #   desc "#{command.capitalize} sidekiq"
+  #   task command do
+  #     # need to get rid of sudo here: have to figure out how it works with upstart
+  #     invoke :sudo
+  #     queue "sudo service #{sidekiq_name} #{command}"
+  #     queue  %[echo "-----> #{command.capitalize} sidekiq."]
+  #   end
+  # end
 end
