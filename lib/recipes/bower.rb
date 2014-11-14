8dd00bddf0d40976f3a94fcdb69da51e58987bf2
Igor Davydov <iskiche@gmail.com>
2014-11-14 03:07:17 +0300

    run bower always

--- lib/recipes/bower.rb
+++ lib/recipes/bower.rb
@@ -11,31 +11,6 @@ namespace :bower do
 
   desc "Install assets"
   task :install_assets do
-    if ENV['force_bower']
-      invoke :'bower:install_assets:force'
-    else
-      message = verbose_mode? ?
-        '$((count)) changes found, installing asets database' :
-        'bower install'
-
-      queue check_for_changes_script \
-        :check => 'bower.json',
-        :at => ['bower.json'],
-        :skip => %[
-          echo "-----> bower.json unchanged; skipping"
-        ],
-        :changed => %[
-          echo "-----> #{message}"
-          bower install
-        ],
-        :default => %[
-          echo echo "-----> Installing Assets with Bower..."
-          bower install
-        ]
-    end
-  end
-
-  task :'install_assets:force' do
     queue %{echo "-----> Installing Assets with Bower..."}
     queue "bower install"
   end
