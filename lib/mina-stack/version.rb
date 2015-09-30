9708c41f85566c4063986e5a59ffb9e42ad7b39c
Igor Davydov <iskiche@gmail.com>
2015-10-01 01:02:06 +0300

    reload sidekiq

--- lib/mina-stack/version.rb
+++ lib/mina-stack/version.rb
@@ -1,5 +1,5 @@
 module Mina
   module Stack
-    VERSION = "0.0.12"
+    VERSION = "0.0.13"
   end
 end
--- lib/recipes/sidekiq.rb
+++ lib/recipes/sidekiq.rb
@@ -17,7 +17,7 @@ namespace :sidekiq do
 
   desc "Quiet sidekiq (stop accepting new work)"
   task :quiet => :environment do
-    queue "pgrep -f '#{sidekiq_name}' | xargs kill -USR1"
+    queue "sudo reload #{sidekiq_name}"
     queue  %[echo "-----> Quiet sidekiq (stop accepting new work)."]
   end
 
