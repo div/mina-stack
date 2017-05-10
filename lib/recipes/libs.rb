44b390d025bcc91fca830477d140ddba50d3cd0f
Josh Brody <josh@josh.mn>
2017-05-10 15:17:55 -0500

    Update libs.rb
    add sudo apt-get install libpq-dev for pg

--- lib/recipes/libs.rb
+++ lib/recipes/libs.rb
@@ -6,7 +6,7 @@ namespace :libs do
     queue "sudo apt-get -y install curl libcurl3 libcurl3-dev"
     queue "sudo apt-get -y install libxml2 libxml2-dev libxslt-dev"
     queue "sudo apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libreadline-dev libyaml-dev libffi-dev"
-
+    queue "sudo apt-get -y install libpq-dev"
   end
 
   task(:setup) {  }
