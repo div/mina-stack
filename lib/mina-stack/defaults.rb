5d339fe55ca04ea237ed299ad2f8003c1d6ded87
Josh Brody <josh@josh.mn>
2017-05-11 19:19:08 -0500

    change set default to set

--- lib/mina-stack/defaults.rb
+++ lib/mina-stack/defaults.rb
@@ -1,78 +1,78 @@
 task :defaults do
-  set_default :ruby_version,          "2.3.1"
-  set_default :services_path,         "/etc/init.d"
-  set_default :upstart_path,          "/etc/init"
-  set_default :tmp_path,              "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/tmp"
-  set_default :sockets_path,          "#{fetch(:tmp_path)}/sockets"
-  set_default :pids_path,             "#{fetch(:tmp_path)}/pids"
-  set_default :logs_path,             "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/log"
-  set_default :config_path,           "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/config"
-  set_default :app_namespace,         "#{fetch(:app!)}_#{fetch(:rails_env!)}"
-  set_default :bundle,                "cd #{fetch(:deploy_to)}/#{fetch(:current_path)} && #{fetch(:bundle_bin)}"
+  set :ruby_version,          "2.3.1"
+  set :services_path,         "/etc/init.d"
+  set :upstart_path,          "/etc/init"
+  set :tmp_path,              "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/tmp"
+  set :sockets_path,          "#{fetch(:tmp_path)}/sockets"
+  set :pids_path,             "#{fetch(:tmp_path)}/pids"
+  set :logs_path,             "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/log"
+  set :config_path,           "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/config"
+  set :app_namespace,         "#{fetch(:app!)}_#{fetch(:rails_env!)}"
+  set :bundle,                "cd #{fetch(:deploy_to)}/#{fetch(:current_path)} && #{fetch(:bundle_bin)}"
 
-  set_default :term_mode,             :pretty
+  set :term_mode,             :pretty
 
-  set_default :psql_user,             "#{fetch(:app!)}"
-  set_default :psql_database,         "#{fetch(:app_namespace)}"
-  set_default :postgresql_version,    "9.6"
-  set_default :postgresql_pid,        "/var/run/postgresql/#{fetch(:postgresql_version)}-main.pid"
+  set :psql_user,             "#{fetch(:app!)}"
+  set :psql_database,         "#{fetch(:app_namespace)}"
+  set :postgresql_version,    "9.6"
+  set :postgresql_pid,        "/var/run/postgresql/#{fetch(:postgresql_version)}-main.pid"
 
-  set_default :nginx_client_max_body_size, "4M"
-  set_default :cloudflare_ssl,        true
+  set :nginx_client_max_body_size, "4M"
+  set :cloudflare_ssl,        true
 
-  set_default :memcached_pid,         "/var/run/memcached.pid"
+  set :memcached_pid,         "/var/run/memcached.pid"
 
-  set_default :unicorn_name,          "unicorn_#{fetch(:app_namespace!)}"
-  set_default :unicorn_socket,        "#{fetch(:sockets_path)}/unicorn.sock"
-  set_default :unicorn_pid,           "#{fetch(:pids_path)}/unicorn.pid"
-  set_default :unicorn_config,        "#{fetch(:config_path)}/unicorn.rb"
-  set_default :unicorn_log,           "#{fetch(:logs_path)}/unicorn.log"
-  set_default :unicorn_error_log,     "#{fetch(:logs_path)}/unicorn.error.log"
-  set_default :unicorn_script,        "#{fetch(:services_path!)}/#{fetch(:unicorn_name)}"
-  set_default :unicorn_workers,       1
-  set_default :unicorn_bin,           lambda { "#{fetch(:bundle_bin)} exec unicorn" }
-  set_default :unicorn_cmd,           "cd #{fetch(:deploy_to)}/#{fetch(:current_path)} && #{fetch(:unicorn_bin)} -D -c #{fetch(:unicorn_config)} -E #{fetch(:rails_env)}"
-  set_default :unicorn_user,          fetch(:user)
-  set_default :unicorn_group,         fetch(:user)
+  set :unicorn_name,          "unicorn_#{fetch(:app_namespace!)}"
+  set :unicorn_socket,        "#{fetch(:sockets_path)}/unicorn.sock"
+  set :unicorn_pid,           "#{fetch(:pids_path)}/unicorn.pid"
+  set :unicorn_config,        "#{fetch(:config_path)}/unicorn.rb"
+  set :unicorn_log,           "#{fetch(:logs_path)}/unicorn.log"
+  set :unicorn_error_log,     "#{fetch(:logs_path)}/unicorn.error.log"
+  set :unicorn_script,        "#{fetch(:services_path!)}/#{fetch(:unicorn_name)}"
+  set :unicorn_workers,       1
+  set :unicorn_bin,           lambda { "#{fetch(:bundle_bin)} exec unicorn" }
+  set :unicorn_cmd,           "cd #{fetch(:deploy_to)}/#{fetch(:current_path)} && #{fetch(:unicorn_bin)} -D -c #{fetch(:unicorn_config)} -E #{fetch(:rails_env)}"
+  set :unicorn_user,          fetch(:user)
+  set :unicorn_group,         fetch(:user)
 
-  set_default :nginx_pid,             "/var/run/nginx.pid"
-  set_default :nginx_config,          "#{fetch(:nginx_path!)}/sites-available/#{fetch(:app_namespace!)}.conf"
-  set_default :nginx_config_e,        "#{fetch(:nginx_path!)}/sites-enabled/#{fetch(:app_namespace!)}.conf"
+  set :nginx_pid,             "/var/run/nginx.pid"
+  set :nginx_config,          "#{fetch(:nginx_path!)}/sites-available/#{fetch(:app_namespace!)}.conf"
+  set :nginx_config_e,        "#{fetch(:nginx_path!)}/sites-enabled/#{fetch(:app_namespace!)}.conf"
 
-  set_default :sidekiq_name,          "sidekiq_#{fetch(:app_namespace!)}"
-  set_default :sidekiq_cmd,           lambda { "#{fetch(:bundle_bin)} exec sidekiq" }
-  set_default :sidekiqctl_cmd,        lambda { "#{fetch(:bundle_prefix)} sidekiqctl" }
-  set_default :sidekiq_timeout,       10
-  set_default :sidekiq_config,        "#{fetch(:config_path)}/sidekiq.yml"
-  set_default :sidekiq_log,           "#{fetch(:logs_path)}/sidekiq.log"
-  set_default :sidekiq_pid,           "#{fetch(:pids_path)}/sidekiq.pid"
-  set_default :sidekiq_concurrency,   25
-  set_default :sidekiq_start,         "#{fetch(:sidekiq_cmd)} -e #{fetch(:rails_env)} -C #{fetch(:sidekiq_config)}"
-  set_default :sidekiq_upstart,       "#{fetch(:upstart_path!)}/#{fetch(:sidekiq_name)}.conf"
+  set :sidekiq_name,          "sidekiq_#{fetch(:app_namespace!)}"
+  set :sidekiq_cmd,           lambda { "#{fetch(:bundle_bin)} exec sidekiq" }
+  set :sidekiqctl_cmd,        lambda { "#{fetch(:bundle_prefix)} sidekiqctl" }
+  set :sidekiq_timeout,       10
+  set :sidekiq_config,        "#{fetch(:config_path)}/sidekiq.yml"
+  set :sidekiq_log,           "#{fetch(:logs_path)}/sidekiq.log"
+  set :sidekiq_pid,           "#{fetch(:pids_path)}/sidekiq.pid"
+  set :sidekiq_concurrency,   25
+  set :sidekiq_start,         "#{fetch(:sidekiq_cmd)} -e #{fetch(:rails_env)} -C #{fetch(:sidekiq_config)}"
+  set :sidekiq_upstart,       "#{fetch(:upstart_path!)}/#{fetch(:sidekiq_name)}.conf"
 
-  set_default :private_pub_name,      "private_pub_#{fetch(:app_namespace)}"
-  set_default :private_pub_cmd,       lambda { "#{fetch(:bundle_prefix)} rackup private_pub.ru" }
-  set_default :private_pub_pid,       "#{fetch(:pids_path)}/private_pub.pid"
-  set_default :private_pub_config,    "#{fetch(:config_path)}/private_pub.yml"
-  set_default :private_pub_log,       "#{fetch(:logs_path)}/private_pub.log"
+  set :private_pub_name,      "private_pub_#{fetch(:app_namespace)}"
+  set :private_pub_cmd,       lambda { "#{fetch(:bundle_prefix)} rackup private_pub.ru" }
+  set :private_pub_pid,       "#{fetch(:pids_path)}/private_pub.pid"
+  set :private_pub_config,    "#{fetch(:config_path)}/private_pub.yml"
+  set :private_pub_log,       "#{fetch(:logs_path)}/private_pub.log"
 
-  set_default :rpush_name,            "rpush_#{fetch(:app_namespace!)}"
-  set_default :rpush_cmd,             lambda { "#{fetch(:bundle_bin)} exec rpush" }
-  set_default :rpush_upstart,         "#{fetch(:upstart_path!)}/#{fetch(:rpush_name)}.conf"
-  set_default :rpush_start,           "#{fetch(:rpush_cmd)} start -f -e #{fetch(:rails_env)}"
+  set :rpush_name,            "rpush_#{fetch(:app_namespace!)}"
+  set :rpush_cmd,             lambda { "#{fetch(:bundle_bin)} exec rpush" }
+  set :rpush_upstart,         "#{fetch(:upstart_path!)}/#{fetch(:rpush_name)}.conf"
+  set :rpush_start,           "#{fetch(:rpush_cmd)} start -f -e #{fetch(:rails_env)}"
 
-  set_default :monit_config_path,     "/etc/monit/conf.d"
-  set_default :monit_http_port,       2812
-  set_default :monit_http_username,   "PleaseChangeMe_monit"
-  set_default :monit_http_password,   "PleaseChangeMe"
+  set :monit_config_path,     "/etc/monit/conf.d"
+  set :monit_http_port,       2812
+  set :monit_http_username,   "PleaseChangeMe_monit"
+  set :monit_http_password,   "PleaseChangeMe"
 
-  set_default :shared_paths,          %w(
+  set :shared_paths,          %w(
                                         tmp
                                         log
                                         public/uploads
                                       )
 
-  set_default :monitored,             %w(
+  set :monitored,             %w(
                                         nginx
                                         postgresql
                                         redis
@@ -82,7 +82,7 @@ task :defaults do
                                         memcached
                                         )
 
-  set_default :server_stack,          %w(
+  set :server_stack,          %w(
                                         nginx
                                         postgresql
                                         redis
