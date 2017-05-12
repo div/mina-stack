0f2c3d3fa7275e25db440fcf6f82e98271430729
Josh Brody <josh@josh.mn>
2017-05-12 14:45:02 -0500

    i don't even know what i just updated besides adding pg stuff and fixing calc

--- lib/mina-stack/pgcalc.rb
+++ lib/mina-stack/pgcalc.rb
@@ -1,54 +1,54 @@
 class Pgcalc
 
-def initialize(ram, connections)
-	@ram = ram
-	@ram_in_kb = ram * 1073741824
-	@connections = connections
-	@settings = {
-	max_connections: @connections,
-shared_buffers: (@ram_in_kb.to_f / 4.to_f).floor.to_i / 1048576,
-effective_cache_size: (@ram_in_kb.to_f * 0.75).floor.to_i / 1048576,
-work_mem: 0,
-maintenance_work_mem: (@ram_in_kb.to_f / 16.to_f).floor.to_i / 1048576,
-min_wal_size: '1GB',
-max_wal_size: '2GB',
-checkpoint_completion_target: '0.7',
-wal_buffers: 0,
-default_statistics_target: '100'
-}
-@metrics = { 'MB': [:shared_buffers, :effective_cache_size, :maintenance_work_mem], 'kB': [:wal_buffers, :work_mem] }
-calculate 
-end
-
-def calculate
-	wal_buffers = ((0.03 * @settings[:shared_buffers].to_f) * 1024).to_i
-	if wal_buffers > 15000
-		@settings[:wal_buffers] = '16MB'
-	else
-		@settings[:wal_buffers] = wal_buffers
+	def initialize(ram, connections)
+		@ram = ram
+		@ram_in_kb = ram * 1073741824
+		@connections = connections
+		@settings = {
+				max_connections: @connections,
+				shared_buffers: (@ram_in_kb.to_f / 4.to_f).floor.to_i / 1048576,
+				effective_cache_size: (@ram_in_kb.to_f * 0.75).floor.to_i / 1048576,
+				work_mem: 0,
+				maintenance_work_mem: (@ram_in_kb.to_f / 16.to_f).floor.to_i / 1048576,
+				min_wal_size: '1GB',
+				max_wal_size: '2GB',
+				checkpoint_completion_target: '0.7',
+				wal_buffers: 0,
+				default_statistics_target: '100'
+		}
+		@metrics = { 'MB': [:shared_buffers, :effective_cache_size, :maintenance_work_mem], 'kB': [:wal_buffers, :work_mem] }
+		calculate
 	end
-	work_mem = (@ram_in_kb - @settings[:shared_buffers]) / 2 
-	@settings[:work_mem] = work_mem / (@settings[:max_connections] * 2) / 1024 
-end 
 
-def to_s
-	s = []
-	@settings.each do |k,v|
-		str = "#{k} = #{v}"
-		if ['kb', 'gb', 'mb'].include?(str.downcase[-2, 2])
-			s << str
-			next
+	def calculate
+		wal_buffers = ((0.03 * @settings[:shared_buffers].to_f) * 1024).to_i
+		if wal_buffers > 15000
+			@settings[:wal_buffers] = '16MB'
+		else
+			@settings[:wal_buffers] = wal_buffers
 		end
-		met = nil 
-		@metrics.each do |m, ary|
+		work_mem = (@ram_in_kb - @settings[:shared_buffers]) / 2
+		@settings[:work_mem] = work_mem / (@settings[:max_connections] * 2) / 1024
+	end
+
+	def to_s
+		s = []
+		@settings.each do |k,v|
+			str = "#{k} = #{v}"
+			if ['kb', 'gb', 'mb'].include?(str.downcase[-2, 2])
+				s << str
+				next
+			end
+			met = nil
+			@metrics.each do |m, ary|
 				if ary.include?(k)
-					met = m 
-					break 
+					met = m
+					break
 				end
 			end
-		s << "#{str}#{met}" 
+			s << "#{str}#{met}"
+		end
+		s.join("\n")
 	end
-	s.join("\n")
-end
 
 end
--- lib/recipes/postgresql.rb
+++ lib/recipes/postgresql.rb
@@ -22,10 +22,10 @@ namespace :postgresql do
   end
 
   desc "Create database"
-  task :create_db do
+  task :create_db => :environment do
     comment "Create database."
     invoke :sudo
-    psql_password = ask "PostgreSQL password:"
+    psql_password = ask "PostgreSQL password (could use #{SecureRandom.hex})"
     set :psql_password, psql_password.strip
     command echo_cmd %{sudo -u postgres psql -c "create user #{fetch(:psql_user)} with password '#{fetch(:psql_password)}';"}
     command echo_cmd %{sudo -u postgres psql -c "create database #{fetch(:psql_database)} owner #{fetch(:psql_user)} --template=template0 ENCODING 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8'";}
@@ -34,18 +34,55 @@ namespace :postgresql do
     template "database.yml.erb"
     tune = ask "Do you want to tune your database? [Y/n]"
     unless tune == 'n'
-      pg_ram = ask "How many GB do you want to provision to the PostgreSQL database?"
-      pg_ram = pg_ram.to_i
-      pg_connections = ask "And max connections to the PostgreSQL database?"
-      pg_connections = pg_connections.to_i
+      invoke :'postgresql:tune'
+    end
+  end
 
-      comment "Tuning database."
-      conf = Pgcalc.new(pg_ram, pg_connections).to_s.split("\n").join('\n')
-      command %{sudo sh -c "echo '#{conf}' >> /etc/postgresql/#{fetch(:postgresql_version)}/main/postgresql.conf"}
-      command "Tuned. Restarting postgresql service."
+
+  desc "Create User"
+  task :create_user => :environment do
+    comment "Create User"
+    invoke :sudo
+    psql_password = ask "Password for PostgreSQL user if you don't want to use an automatically-generated one:"
+    if psql_password.blank?
+      psql_password = SecureRandom.hex
+      puts "Using #{psql_password}"
+    end
+    set :psql_password, psql_password.strip
+    command echo_cmd %{sudo -u postgres psql -c "create user #{fetch(:psql_user)} with password '#{fetch(:psql_password)}';"}
+  end
+
+  task :tune do
+    pg_ram = ask "How many GB do you want to provision to the PostgreSQL database?"
+    pg_ram = pg_ram.to_i
+    pg_connections = ask "And max connections to the PostgreSQL database?"
+    pg_connections = pg_connections.to_i
+
+    comment "Tuning database."
+    conf = Pgcalc.new(pg_ram, pg_connections).to_s.split("\n").join('\n')
+    command %{sudo sh -c "echo '#{conf}' >> /etc/postgresql/#{fetch(:postgresql_version)}/main/postgresql.conf"}
+    restart = ask "Tuned. Do you want to restart the postgresql service now? [y/N]"
+    if restart == 'y'
       command echo_cmd %{sudo service postgresql restart}
     end
   end
+
+  desc "Drop database"
+  task :drop_db do
+    comment "Drop database."
+    invoke :sudo
+    psql_db = ask "PostgreSQL database to drop:"
+    command echo_cmd %{sudo -u postgres psql -c "drop database #{psql_db}";}
+  end
+
+  desc "Drop user"
+  task :drop_user do
+    comment "Drop user."
+    invoke :sudo
+    psql_user = ask "PostgreSQL user to drop:"
+    command echo_cmd %{sudo -u postgres psql -c "drop user #{psql_user}";}
+  end
+
 
   RYAML = <<-BASH
   function ryaml {
