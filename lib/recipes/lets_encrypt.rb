92865aefaa740659c782114dee25442537a2e5ea
Josh Brody <josh@josh.mn>
2017-06-05 16:25:35 -0500

    fix let's encrypt

--- lib/recipes/lets_encrypt.rb
+++ lib/recipes/lets_encrypt.rb
@@ -1,4 +1,5 @@
 namespace :lets_encrypt do
+  require 'highline/import'
 
   desc "Install Let's Encrypt"
   task :install => :environment do
@@ -6,5 +7,11 @@ namespace :lets_encrypt do
     command "add-apt-repository ppa:certbot/certbot -y"
     command "apt-get update"
     command "apt-get install certbot -y"
+    unless fetch(:lets_encrypt_email).present?
+      lets_encrypt_email = ask "What email do you want to use for Let's Encrypt?"
+    end
+    set :lets_encrypt_email, lets_encrypt_email.strip
+    command "sudo certbot certonly -a webroot --email #{fetch(:lets_encrypt_email)} --webroot-path=/usr/share/nginx/html -d #{fetch(:server_name)}"
+    command "If successful, you should set `set :lets_encrypt, true` in your mina deploy environment and then run `mina nginx:setup`"
   end
 end
